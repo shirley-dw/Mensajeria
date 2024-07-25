4ee8dd5b366cb43ed0437567a203ec59dd1b0d86
shirley-dw <vokacshirley@gmail.com>
2024-07-25 20:22:19 -0300

    Agrego contactos en mensajeria.json

--- src/Components/Contactos/Contacto/Contacto.jsx
+++ src/Components/Contactos/Contacto/Contacto.jsx
@@ -6,27 +6,29 @@ import "./Contacto.css";
 
 
 /* Componente Contacto le paso props a contacto */
-export const Contacto = ({ id, nombre, thumbnail, mensajes }) => {
-/* Desestructuro la props contacto */
+const Contacto = ({ id, nombre, thumbnail, mensajes }) => {
+  const imagenes = '/imagenes/' + thumbnail;
 
-  const imagenes = '/imagenes/' + thumbnail 
+  const ultimoMensaje = mensajes.length > 0 ? mensajes[mensajes.length - 1] : null;
+  const textoUltimoMensaje = ultimoMensaje?.text ?? 'Sin mensajes';
+  const horaUltimoMensaje = ultimoMensaje?.hour;
 
-  const ultimoMensaje= mensajes[mensajes.length - 1] /* Ultimo mensaje en el array mensajeID */
-  const horaUltimoMensaje = ultimoMensaje?.hour/* Hora del ultimo mensaje */
-
-  return (/* Return devuelve elementos JSX. */
+  return (
     <Link className="contact-link" to={`/mensaje/${id}`}>
-      <div key={id} className="contact-item">{/* Link react-router-dom crea un enlace que lleva a una ruta específicada con to la URL a la que debe dirigir el enlace en él que concatena la cadena /mensaje/con el valor de la variable. */}
+      <div key={id} className="contact-item">
         <img src={imagenes} alt={nombre} className="img" />
         <div className="dato">
           <p className="name">
             <strong>{nombre}</strong>
           </p>
+          <div className="ultimo-mensaje">{textoUltimoMensaje}</div>
         </div>
         <div className="time">{horaUltimoMensaje}</div>
+        
+        {/* Utiliza la variable ultimoMensaje para mostrar el texto del último mensaje */}
       </div>
     </Link>
-  )
-}
+  );
+};
 
 export default Contacto;
--- src/Components/Contactos/ListaContactos/ListaContactos.jsx
+++ src/Components/Contactos/ListaContactos/ListaContactos.jsx
@@ -22,21 +22,6 @@ import { ObtenerContactos }from '../../../Fetching/contactosFetching.js'
         console.error("Error al obtener contactos:", error);
       });
   }, []);
-  useEffect(() => {
-    const fetchContactos = async () => {
-      try {
-        const contactos = await ObtenerContactos();
-        contactos.forEach((contacto) => {
-          const ultimoMensaje = contacto.mensajes[contacto.mensajes.length - 1];
-          contacto.ultimoMensaje = ultimoMensaje;
-        });
-        setContactos(contactos);J
-      } catch (error) {
-        console.error("Error al obtener contactos:", error);
-      }
-    };
-    fetchContactos();
-  }, []);
 
   useEffect(() => {
     if (search) {
